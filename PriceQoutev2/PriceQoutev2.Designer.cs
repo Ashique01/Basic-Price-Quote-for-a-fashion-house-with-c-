0eeba8906d52894f71a7bb0f78061301d783f03a
Ashique01 <ashiquemura@gmail.com>
2024-08-24 19:44:49 +0600

    full project has committed

---
+++ PriceQoutev2/PriceQoutev2.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace PriceQoutev2
+{
+    partial class PriceQoutev2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PriceQoutev2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.TxTradeIn = new System.Windows.Forms.TextBox();
+            this.picCompute = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RdobestQuality = new System.Windows.Forms.RadioButton();
+            this.RdobetterQualty = new System.Windows.Forms.RadioButton();
+            this.RdogoodQuality = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RdopremiumPkg = new System.Windows.Forms.RadioButton();
+            this.RdostandardPkg = new System.Windows.Forms.RadioButton();
+            this.btnClearInput = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblBasePrice = new System.Windows.Forms.Label();
+            this.lblPkgPrice = new System.Windows.Forms.Label();
+            this.lblSubTotal = new System.Windows.Forms.Label();
+            this.lblSalesTax = new System.Windows.Forms.Label();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCompute)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(156, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AM Fashion LTD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Base Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Package Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Trade In";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(50, 282);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Sub Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 336);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 13);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "Sales Tax";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(50, 391);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 13);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Total Price";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(141, 87);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(142, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // TxTradeIn
+            // 
+            this.TxTradeIn.Location = new System.Drawing.Point(141, 226);
+            this.TxTradeIn.Name = "TxTradeIn";
+            this.TxTradeIn.Size = new System.Drawing.Size(142, 20);
+            this.TxTradeIn.TabIndex = 3;
+            // 
+            // picCompute
+            // 
+            this.picCompute.Image = ((System.Drawing.Image)(resources.GetObject("picCompute.Image")));
+            this.picCompute.Location = new System.Drawing.Point(229, 444);
+            this.picCompute.Name = "picCompute";
+            this.picCompute.Size = new System.Drawing.Size(26, 30);
+            this.picCompute.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCompute.TabIndex = 4;
+            this.picCompute.TabStop = false;
+            this.picCompute.Click += new System.EventHandler(this.picCompute_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RdobestQuality);
+            this.groupBox1.Controls.Add(this.RdobetterQualty);
+            this.groupBox1.Controls.Add(this.RdogoodQuality);
+            this.groupBox1.Location = new System.Drawing.Point(329, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(136, 134);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Choose Base Price";
+            // 
+            // RdobestQuality
+            // 
+            this.RdobestQuality.AutoSize = true;
+            this.RdobestQuality.Location = new System.Drawing.Point(18, 94);
+            this.RdobestQuality.Name = "RdobestQuality";
+            this.RdobestQuality.Size = new System.Drawing.Size(83, 17);
+            this.RdobestQuality.TabIndex = 2;
+            this.RdobestQuality.TabStop = true;
+            this.RdobestQuality.Text = "Best Fabrics";
+            this.RdobestQuality.UseVisualStyleBackColor = true;
+            this.RdobestQuality.CheckedChanged += new System.EventHandler(this.bestQuality_CheckedChanged);
+            // 
+            // RdobetterQualty
+            // 
+            this.RdobetterQualty.AutoSize = true;
+            this.RdobetterQualty.Location = new System.Drawing.Point(18, 64);
+            this.RdobetterQualty.Name = "RdobetterQualty";
+            this.RdobetterQualty.Size = new System.Drawing.Size(90, 17);
+            this.RdobetterQualty.TabIndex = 1;
+            this.RdobetterQualty.TabStop = true;
+            this.RdobetterQualty.Text = "Better Fabrics";
+            this.RdobetterQualty.UseVisualStyleBackColor = true;
+            this.RdobetterQualty.CheckedChanged += new System.EventHandler(this.betterQualty_CheckedChanged);
+            // 
+            // RdogoodQuality
+            // 
+            this.RdogoodQuality.AutoSize = true;
+            this.RdogoodQuality.Location = new System.Drawing.Point(18, 31);
+            this.RdogoodQuality.Name = "RdogoodQuality";
+            this.RdogoodQuality.Size = new System.Drawing.Size(88, 17);
+            this.RdogoodQuality.TabIndex = 0;
+            this.RdogoodQuality.TabStop = true;
+            this.RdogoodQuality.Text = "Good Fabrics";
+            this.RdogoodQuality.UseVisualStyleBackColor = true;
+            this.RdogoodQuality.CheckedChanged += new System.EventHandler(this.goodQuality_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.RdopremiumPkg);
+            this.groupBox2.Controls.Add(this.RdostandardPkg);
+            this.groupBox2.Location = new System.Drawing.Point(329, 249);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(136, 90);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Choose Package";
+            // 
+            // RdopremiumPkg
+            // 
+            this.RdopremiumPkg.AutoSize = true;
+            this.RdopremiumPkg.Location = new System.Drawing.Point(18, 52);
+            this.RdopremiumPkg.Name = "RdopremiumPkg";
+            this.RdopremiumPkg.Size = new System.Drawing.Size(65, 17);
+            this.RdopremiumPkg.TabIndex = 1;
+            this.RdopremiumPkg.TabStop = true;
+            this.RdopremiumPkg.Text = "Premium";
+            this.RdopremiumPkg.UseVisualStyleBackColor = true;
+            this.RdopremiumPkg.CheckedChanged += new System.EventHandler(this.premiumPkg_CheckedChanged);
+            // 
+            // RdostandardPkg
+            // 
+            this.RdostandardPkg.AutoSize = true;
+            this.RdostandardPkg.Location = new System.Drawing.Point(18, 29);
+            this.RdostandardPkg.Name = "RdostandardPkg";
+            this.RdostandardPkg.Size = new System.Drawing.Size(68, 17);
+            this.RdostandardPkg.TabIndex = 0;
+            this.RdostandardPkg.TabStop = true;
+            this.RdostandardPkg.Text = "Standard";
+            this.RdostandardPkg.UseVisualStyleBackColor = true;
+            this.RdostandardPkg.CheckedChanged += new System.EventHandler(this.standardPkg_CheckedChanged);
+            // 
+            // btnClearInput
+            // 
+            this.btnClearInput.Location = new System.Drawing.Point(373, 391);
+            this.btnClearInput.Name = "btnClearInput";
+            this.btnClearInput.Size = new System.Drawing.Size(75, 23);
+            this.btnClearInput.TabIndex = 7;
+            this.btnClearInput.Text = "Clear";
+            this.btnClearInput.UseVisualStyleBackColor = true;
+            this.btnClearInput.Click += new System.EventHandler(this.btnClearInput_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(373, 430);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 8;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(212, 425);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(62, 16);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Compute";
+            // 
+            // lblBasePrice
+            // 
+            this.lblBasePrice.AutoSize = true;
+            this.lblBasePrice.Location = new System.Drawing.Point(148, 136);
+            this.lblBasePrice.Name = "lblBasePrice";
+            this.lblBasePrice.Size = new System.Drawing.Size(0, 13);
+            this.lblBasePrice.TabIndex = 10;
+            // 
+            // lblPkgPrice
+            // 
+            this.lblPkgPrice.AutoSize = true;
+            this.lblPkgPrice.Location = new System.Drawing.Point(148, 181);
+            this.lblPkgPrice.Name = "lblPkgPrice";
+            this.lblPkgPrice.Size = new System.Drawing.Size(0, 13);
+            this.lblPkgPrice.TabIndex = 11;
+            // 
+            // lblSubTotal
+            // 
+            this.lblSubTotal.AutoSize = true;
+            this.lblSubTotal.Location = new System.Drawing.Point(148, 282);
+            this.lblSubTotal.Name = "lblSubTotal";
+            this.lblSubTotal.Size = new System.Drawing.Size(0, 13);
+            this.lblSubTotal.TabIndex = 12;
+            // 
+            // lblSalesTax
+            // 
+            this.lblSalesTax.AutoSize = true;
+            this.lblSalesTax.Location = new System.Drawing.Point(148, 336);
+            this.lblSalesTax.Name = "lblSalesTax";
+            this.lblSalesTax.Size = new System.Drawing.Size(0, 13);
+            this.lblSalesTax.TabIndex = 13;
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Location = new System.Drawing.Point(148, 391);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(0, 13);
+            this.lblTotalPrice.TabIndex = 14;
+            // 
+            // PriceQoutev2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 486);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblSalesTax);
+            this.Controls.Add(this.lblSubTotal);
+            this.Controls.Add(this.lblPkgPrice);
+            this.Controls.Add(this.lblBasePrice);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClearInput);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picCompute);
+            this.Controls.Add(this.TxTradeIn);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "PriceQoutev2";
+            this.Text = "AM FAshion Price Quote";
+            this.Load += new System.EventHandler(this.PriceQoutev2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCompute)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox TxTradeIn;
+        private System.Windows.Forms.PictureBox picCompute;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnClearInput;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.RadioButton RdobestQuality;
+        private System.Windows.Forms.RadioButton RdobetterQualty;
+        private System.Windows.Forms.RadioButton RdogoodQuality;
+        private System.Windows.Forms.RadioButton RdopremiumPkg;
+        private System.Windows.Forms.RadioButton RdostandardPkg;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblBasePrice;
+        private System.Windows.Forms.Label lblPkgPrice;
+        private System.Windows.Forms.Label lblSubTotal;
+        private System.Windows.Forms.Label lblSalesTax;
+        private System.Windows.Forms.Label lblTotalPrice;
+    }
+}
+
---
+++ PriceQoutev2/PriceQoutev2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PriceQoutev2
+{
+    public partial class PriceQoutev2 : Form
+    {
+        public PriceQoutev2()
+        {
+            InitializeComponent();
+        }
+
+        double BaseGoodQuality, BaseBetterQuality, BaseBestQuality, PackageStandard, PackagePremium;
+        double basePrice, pkgPrice, tradeIn, subTotal, salesTax, totalPrice;
+
+       
+        private void PriceQoutev2_Load(object sender, EventArgs e)
+        {
+            BaseGoodQuality = 500;
+            BaseBetterQuality = 1200;
+            BaseBestQuality = 2000;
+
+            PackageStandard = 500;
+            PackagePremium = 1500;
+
+            basePrice = 0;
+            pkgPrice = 0;
+            tradeIn = '0';
+        }
+        private void goodQuality_CheckedChanged(object sender, EventArgs e)
+        {
+            lblBasePrice.Text = BaseGoodQuality.ToString("c");
+            basePrice = BaseGoodQuality;
+        }
+
+        private void betterQualty_CheckedChanged(object sender, EventArgs e)
+        {
+            lblBasePrice.Text = BaseBetterQuality.ToString("c");
+            basePrice = BaseBetterQuality;
+        }
+        private void bestQuality_CheckedChanged(object sender, EventArgs e)
+        {
+            lblBasePrice.Text = BaseBestQuality.ToString("c");
+            basePrice = BaseBestQuality;
+        }
+        private void standardPkg_CheckedChanged(object sender, EventArgs e)
+        {
+            lblPkgPrice.Text = PackageStandard.ToString("C");
+            pkgPrice = PackageStandard;
+        }
+
+        private void premiumPkg_CheckedChanged(object sender, EventArgs e)
+        {
+            lblPkgPrice.Text = PackagePremium.ToString("c");
+            pkgPrice = PackagePremium;
+        }
+
+        private void btnClearInput_Click(object sender, EventArgs e)
+        {
+            RdogoodQuality.Checked = false;
+            RdobetterQualty.Checked = false;
+            RdobestQuality.Checked = false;
+
+            RdostandardPkg.Checked = false;
+            RdopremiumPkg.Checked = false;
+
+            txtName.Text = "";
+
+            lblBasePrice.Text = "";
+            lblPkgPrice.Text = "";
+
+            TxTradeIn.Text = "0";
+
+            lblSubTotal.Text = "";
+            lblSalesTax.Text = "";
+            lblTotalPrice.Text = "";
+
+            basePrice = 0;
+            pkgPrice = 0;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void picCompute_Click(object sender, EventArgs e)
+        {
+            tradeIn = double.Parse(TxTradeIn.Text);
+
+            subTotal = basePrice + pkgPrice - tradeIn;
+            salesTax = subTotal * 0.1;
+            totalPrice = subTotal + salesTax;
+
+            TxTradeIn.Text = tradeIn.ToString("c");
+            lblSubTotal.Text = subTotal.ToString("c");
+            lblSalesTax.Text = salesTax.ToString("c");
+            lblTotalPrice.Text = totalPrice.ToString("c");
+
+
+        }
+
+
+
+    }
+}
